ec803492cb2b99e81ae77c704265b45cac67fc80
user <user@DESKTOP-8MI5VJJ>
2021-04-14 14:16:44 +0900

    21년 4월 14일 오후 2시 수업 과정

---
+++ src/Day11_Studen_pkg/Cal.java
@@ -0,0 +1,24 @@
+package Day11_Studen_pkg;
+
+public class Cal {
+
+	int num1;
+	int num2;
+	
+	void sum1(int num1, int num2) { //( 이 안에 들어간게 매개변수 )
+		System.out.println(this.num1 + this.num2);
+		
+				
+	}
+	
+	int sum2(int num1, int num2) {
+		
+		int sum = num1 + num2;
+		return sum;
+		
+		
+		
+	}
+	
+	
+}
---
+++ src/Day11_Studen_pkg/CalMain.java
@@ -0,0 +1,27 @@
+package Day11_Studen_pkg;
+
+public class CalMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cal cal1 = new Cal();
+		
+		cal1.num1=10;
+		cal1.num2=20;
+		cal1.sum1(0, 0);
+		
+		
+		
+		Cal cal2 = new Cal();
+		
+		int sum3 = cal2.sum2(10,20);
+		System.out.println(sum3);
+		
+		
+		
+		
+		
+		
+	}
+
+}
