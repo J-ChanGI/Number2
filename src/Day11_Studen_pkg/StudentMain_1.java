cfe5ef4c9974d26e8e28b10c146a139b257b6c79
user <user@DESKTOP-8MI5VJJ>
2021-04-14 12:40:17 +0900

    21년 4월 14일 오전 수업

--- src/Day11_Studen_pkg/StudentMain_1.java
+++ src/Day11_Studen_pkg/StudentMain_1.java
@@ -4,7 +4,55 @@ public class StudentMain_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Student_Class_1 stu1 = new Student_Class_1();
+		stu1.name = "학생1";
+		stu1.stuNum = 12345;
+		System.out.println(stu1.name);
+		System.out.println(stu1.stuNum);
+		// 매개변수가 있는 생성자를 이용한 객체선언
+		Student_Class_1 stu2 = new Student_Class_1("학생2", 1111);
+		System.out.println(stu2.name);
+		System.out.println(stu2.stuNum);
+	
+		
+		System.out.println("메소드 호출전"); //1번
+		//  fieldPrint .메소드 호출
+		stu1.fieldPrint();
+		
+		
+		System.out.println("메소드 호출 끝"); //3번 순으로 출력이 된다
+		
+		stu2.fieldPrint();
+		
+		Student_Class_1 stu3 = new Student_Class_1("학생3",12314);
+		stu3.fieldPrint();
+		
+		System.out.println("리턴있는 메소드 호출");
+		// 리턴이 있는 메소드를 호출 할 떄는 호출만하고 끝나는 것이 아니라 
+		// 리턴 값을 활용하는 내용이 필요
+		stu1.numberMethod();
+		
+		int number =0;
+		System.out.println(number); // 메소드 호출전
+		number = stu1.numberMethod();
+		System.out.println(number); // 메소드 호출 후
+		
+		System.out.println("String 메소드 호출");
+		System.out.println(stu3.stringMethod());
+		
+		String bb = stu3.stringMethod();
+		System.out.println(bb);
+		
+		Student_Class_1 stu4 = new Student_Class_1(); 
+			int cc = stu4.numberMethod2(30);
+			System.out.println(cc);
+			
+			
+			//40 
+		
+		
 	}
+	
 
 }
--- src/Day11_Studen_pkg/Student_Class_1.java
+++ src/Day11_Studen_pkg/Student_Class_1.java
@@ -2,4 +2,55 @@ package Day11_Studen_pkg;
 
 public class Student_Class_1 {
 
+	String name;
+	int stuNum;
+	// int 는 0을 받을 수 없기에 숫자 001234 일 경우 1234 만 출력이 된다.
+	//기본생성자 선언
+	Student_Class_1(){
+		System.out.println("기본생성자 호출");
+	}
+	
+	Student_Class_1(String name, int stuNum){
+		System.out.println("매개변수있는 생성자");
+		this.name = name;
+		this.stuNum = stuNum;
+	}
+	// 메소드 선언
+	// 리턴이 없는 메소드 선언
+	/*
+	 * void : 리턴이 없다는 의미
+	 * 메소드 이름  fieldPrint 메소드 이름은 자유롭게 지을 수 있다
+	 * 매게변수 : 없음
+	 */
+	void fieldPrint() { //2번
+		System.out.println("매소드 호출");
+		System.out.println(this.name);
+		System.out.println(this.stuNum);
+	}
+	
+	//리턴이 있는 메소드 선언
+	/*
+	 * int : 리턴되는 값이  int
+	 * 메소드 이름 : numberMathod
+	 * 매개변수 : 없음
+	 */
+	int numberMethod() { // int자리에는 int, String, lovg 등이 올 수 있다.
+		int num =10; //특정 메소드 안에 선언하는건 변수
+		return num; // num 변수가 가진 값을 리턴해준다
+	}
+	
+	String stringMethod() {
+		String va1 = "리턴값";
+		return va1;
+	}
+	// 매개변수가 있는 메소드
+	int numberMethod2(int num1) { //his method must return a result of type int  오류는 반드시 리턴문이 있어야한다. 라는 문구
+		int num2 = 10;
+		int sum = num1 + num2;
+		return sum ;
+		
+	}
+	
 }
+
+	
